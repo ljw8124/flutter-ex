a25216e0a9f5e0d21689192ad62e2af9019ad840
joungwoo <ljw8124@gmail.com>
2023-12-21 09:08:20 +0900

    stateful widget

---
+++ flutter_ex02/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  void onClickEvent() {
+    // 데이터가 변경되었다고 알리는 함수
+    setState(() {
+      counter += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClickEvent,
+                icon: const Icon(
+                  Icons.add_box_outlined,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
