c4593f5a8ca781141d7c5ca2b3cae07be387d388
joungwoo <ljw8124@gmail.com>
2023-12-21 10:32:01 +0900

    context

--- flutter_ex02/lib/main.dart
+++ flutter_ex02/lib/main.dart
@@ -12,54 +12,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 0;
-
-  List<int> numbers = [];
-
-  void onClickEvent() {
-    // 데이터가 변경되었다고 알리는 함수
-    // 이 때 데이터가 변하는 코드를 setState() 안에 넣지 않아도 됨
-    setState(() {
-      counter += 1;
-      numbers.add(numbers.length);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
-              ),
-              // Text(
-              //   '$counter',
-              //   style: const TextStyle(fontSize: 30),
-              // ),
-              for (var n in numbers)
-                Text(
-                  '$n',
-                  style: const TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-
-              IconButton(
-                iconSize: 40,
-                onPressed: onClickEvent,
-                icon: const Icon(
-                  Icons.add_box_outlined,
-                ),
-              )
-            ],
+            children: [MyLargeTitle()],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      ' My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
