455988426eefcdbebbb96a9c2385e72a02062310
joungwoo <ljw8124@gmail.com>
2023-12-22 15:14:10 +0900

    pause 추가, 시계 포맷 추가

--- flutter_ex03/lib/screens/home_screen.dart
+++ flutter_ex03/lib/screens/home_screen.dart
@@ -10,17 +10,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 25 * 60; // 25분 -> 1500초
+  static const twentyFiveMinutes = 15 * 60;
+
+  int totalSeconds = twentyFiveMinutes;
+
+  bool isRunning = false;
+  int totalPomodoros = 0;
   late Timer timer; // non-nullable 변수의 초기화를 늦춤
 
   void onTick(Timer timer) {
-    setState(() {
-      totalSeconds -= 1;
-    });
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros += 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds -= 1;
+      });
+    }
   }
 
   void onStartPressed() {
     timer = Timer.periodic(const Duration(seconds: 1), onTick);
+
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  String clockFormat(int seconds) {
+    var duration = Duration(seconds: seconds);
+
+    String clockString = duration.toString().split('.').first.substring(2, 7);
+
+    return clockString;
   }
 
   @override
@@ -35,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
               // flutter 에서화면 상태표시창 넘어서 정렬하는 방법
               alignment: Alignment.bottomCenter,
               child: Text(
-                '$totalSeconds',
+                clockFormat(totalSeconds),
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -48,9 +83,11 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 3,
             child: Center(
               child: IconButton(
-                onPressed: onStartPressed,
+                onPressed: isRunning ? onPausePressed : onStartPressed,
                 iconSize: 120,
-                icon: const Icon(Icons.play_circle_outline_rounded),
+                icon: isRunning
+                    ? const Icon(Icons.pause_circle_outline_rounded)
+                    : const Icon(Icons.play_circle_outline_rounded),
                 color: Theme.of(context).cardColor,
               ),
             ),
@@ -71,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          'Pomodors',
+                          'Pomodoros',
                           style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.w600,
@@ -82,7 +119,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                         Text(
-                          '0',
+                          '$totalPomodoros',
                           style: TextStyle(
                             fontSize: 60,
                             fontWeight: FontWeight.w600,
